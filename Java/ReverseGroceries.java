17481243614dde08c8bb876ab7fc5c1bbf8ef89f
Jumanazar <jimmy@shinbo.kr>
2021-10-25 15:27:38 +0900

    ReverseGroceries.java was added.

---
+++ Java/ReverseGroceries.java
@@ -0,0 +1,13 @@
+/*
+Definition:
+Reverse list items and returns the reversed list.
+ */
+
+public String[] reverseGroceries(String[] groceryList) {
+    String temp = "";
+    for(int i=0; i < groceryList.length/2; i++){
+        temp = groceryList[i];groceryList[i] = groceryList[groceryList.length - 1 - i];
+        groceryList[groceryList.length - 1 - i] = temp;
+    }
+    return groceryList;
+}
