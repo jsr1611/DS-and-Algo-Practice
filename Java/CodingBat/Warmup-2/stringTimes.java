a9a4226fb6adb90af8a04338519312a1200410ce
Jumanazar <jimmy@shinbo.kr>
2021-10-30 06:16:11 +0900

    Solution was added

---
+++ Java/CodingBat/Warmup-2/stringTimes.java
@@ -0,0 +1,13 @@
+/*
+Author: Jumanazar Saidov
+link: https://codingbat.com/prob/p142270
+Date: 2021-10-30
+ */
+
+public String stringTimes(String str, int n) {
+    String res = "";
+    for(int i=0; i < n; i++){
+        res = res + str;
+    }
+    return res;
+}
