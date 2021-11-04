eb012463eed154acc19be926ba8b81e1e6674de9
Jumanazar <jimmy@shinbo.kr>
2021-11-04 09:12:57 +0900

    Solution was added

---
+++ Java/Generics.java
@@ -0,0 +1,50 @@
+/*
+Author: Jumanazar Saidov
+Date: 2021.11.04
+Link:https://www.hackerrank.com/challenges/30-generics/problem?h_r=email&unlock_token=e3754b7c0d2eebc519f7e428617787c786b5d0ae&utm_campaign=30_days_of_code_continuous&utm_medium=email&utm_source=daily_reminder
+ */
+
+import java.util.*;
+
+class Printer <T> {
+
+    /**
+     *    Method Name: printArray
+     *    Print each element of the generic array on a new line. Do not return anything.
+     *    @param A generic array
+     **/
+
+    // Write your code here
+    public <E> void printArray(E[] array){
+        for(E element : array){
+            System.out.println(element);
+        }
+    }
+
+}
+
+public class Generics {
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+}
