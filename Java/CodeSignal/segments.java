d7657f9cc07fd2f5796b3470cf9ba63f4f53adc2
jimhome_mac <jsr1611@outlook.com>
2021-11-04 22:31:41 +0900

    Problems were added.

---
+++ Java/CodeSignal/segments.java
@@ -0,0 +1,17 @@
+package CodeSignal;
+
+public class segments {
+    int segmentsCovering(int[][] segments) {
+        int l=0, r=0;
+
+        return 3;
+    }
+
+}
+
+
+/*
+You are given a set of closed line segments, represented as a two-dimensional integer array segments. Each segment segments[i] is of the form [l, r], where l is the coordinate of its left endpoint and r is the coordinate of its right endpoint.
+
+Let's say that the segment [l, r] contains the point x if the point is located inside the segment, i.e. l ≤ x ≤ r. Your task is to find the minimal number of points that can be placed somewhere on the number line, so that each segment contains at least one point.
+ */
