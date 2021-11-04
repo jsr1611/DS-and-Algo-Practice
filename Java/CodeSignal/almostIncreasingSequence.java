b0037c57278626cbb98307dd676a06e165cf37ee
Jumanazar <jsr1611@outlook.com>
2021-11-04 17:49:21 +0900

    Solution was added

---
+++ Java/CodeSignal/almostIncreasingSequence.java
@@ -0,0 +1,19 @@
+/*
+Author: Jumanazar Saidov
+Date: 2021.11.04
+Definition:
+Given a sequence of integers as an array, determine whether
+it is possible to obtain a strictly increasing sequence
+by removing no more than one element from the array.
+Link: https://app.codesignal.com/arcade/intro/level-2/2mxbGwLzvkTCKAJMG
+ */
+public class Main2 {
+    public boolean almostIncreasingSequence(int[] sequence){
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Main2 main = new Main2();
+        main.almostIncreasingSequence();
+    }
+}
