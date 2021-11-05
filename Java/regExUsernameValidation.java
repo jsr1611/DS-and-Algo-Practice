0b20f74f7b1ad5dda759526915b46ebca51f74e5
Jumanazar <jsr1611@outlook.com>
2021-11-06 02:22:18 +0900

    Solution was added.

---
+++ Java/regExUsernameValidation.java
@@ -0,0 +1,30 @@
+/*
+Author: Jumanazar Saidov
+Date: 2021.11.06
+Link: https://www.hackerrank.com/challenges/valid-username-checker/problem
+ */
+import java.util.Scanner;
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z$]\\w{7,29}$";
+}
+
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
