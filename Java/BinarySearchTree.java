4a79bd6d0646375304ac93e28b661e94aa066d25
Jumanazar <jsr1611@outlook.com>
2021-11-06 01:12:12 +0900

    Solution was added

---
+++ Java/BinarySearchTree.java
@@ -0,0 +1,63 @@
+/*
+Author: Jumanazar Saidov
+Date: 2021.11.06
+Link: https://www.hackerrank.com/challenges/30-binary-search-trees/problem?utm_campaign=30_days_of_code_continuous&utm_medium=email&utm_source=daily_reminder
+ */
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+    public static int getHeight(Node root){
+        //Write your code here
+        if(root.left == null && root.right != null){
+            return 1 + Math.max(0, getHeight(root.right));
+        }
+        else if(root.right == null && root.left != null){
+            return 1 + Math.max(getHeight(root.left), 0);
+        }
+        if(root.right == null && root.left == null){
+            return 0;
+        }
+        else{
+            return 1+(Math.max(getHeight(root.left), getHeight(root.right)));
+        }
+
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+}
