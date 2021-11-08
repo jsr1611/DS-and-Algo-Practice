daca2aa690a761dc5d82649f17908ca5a49dafdf
Jumanazar <jsr1611@outlook.com>
2021-11-08 21:43:53 +0900

    Solution was added.

---
+++ Java/BinarySearchTraversal_LevelOrderTraversal.java
@@ -0,0 +1,62 @@
+/*
+Author: Jumanazar Saidov
+Date: 2021.11.08
+Link: https://www.hackerrank.com/challenges/30-binary-trees/problem
+ */
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+    static void levelOrder(Node root){
+        //Write your code here
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            Node tempNode = queue.poll();
+            System.out.print(tempNode.data + " " );
+            if(tempNode.left != null){
+                queue.add(tempNode.left);
+            }
+            if(tempNode.right != null){
+                queue.add(tempNode.right);
+            }
+        }
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        levelOrder(root);
+    }
+}
