1e47573f72cf25193b60b4c1be4321d092fac8ff
jimhome_mac <jsr1611@outlook.com>
2021-11-10 01:43:49 +0900

    Solution was added.

---
+++ Java/isProbablePrime.java
@@ -0,0 +1,32 @@
+/*
+Author: Jumanazar Saidov
+Date: 2021.11.10
+Link: https://www.hackerrank.com/challenges/java-primality-test/problem
+ */
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+        import static java.util.stream.Collectors.toList;
+
+
+public class isProbablePrime {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger number = new BigInteger(n);
+        boolean prime = number.isProbablePrime(1);
+
+        String result = prime ? "prime" : "not prime";
+        System.out.println(result);
+        bufferedReader.close();
+    }
+}
+
