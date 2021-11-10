3da0657b12db715aaa89b5545d44f7d445842ed0
Jumanazar <jsr1611@outlook.com>
2021-11-10 12:06:04 +0900

    Solution was added.

---
+++ Java/1D_Array.java
@@ -0,0 +1,30 @@
+/*
+AUthor: Jumanazar Saidov
+Date: 2021.11.10
+Link: https://www.hackerrank.com/challenges/java-1d-array-introduction/problem
+ */
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+
+        int[] a = new int[n];
+        for(int i=0; i < n; i++){
+            int val = scan.nextInt();
+            a[i] = val;
+
+
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
