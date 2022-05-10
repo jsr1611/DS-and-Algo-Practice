4d884a1d2d91c61711659697f21d862f2bffb045
jimhome_mac <jsr1611@outlook.com>
2022-05-11 01:30:30 +0900

    updated solution with improvements

--- Java/Codility/OddOccurrencesInArraySolution.java
+++ Java/Codility/OddOccurrencesInArraySolution.java
@@ -9,31 +9,21 @@ import java.util.*;
 public class OddOccurrencesInArraySolution {
     public static void main(String[] args) {
         OddOccurrencesInArraySolution solution = new OddOccurrencesInArraySolution();
-
-        System.out.println(solution.solution(new int[]{9,3,9,3,9,7,9}));
+        int[] arr = new int[]{9,3,9,3,9};
+        System.out.println(solution.solution(arr));
     }
 
     public int solution(int[] A){
         if(A.length == 1){
             return A[0];
         }
-//        List<Integer> singles = new LinkedList<>();
-        Map<Integer, Integer> nums = new HashMap<>();
-        int sinlge = -1;
-        for (int num : A){
-            if (nums.containsKey(num)){
-                nums.remove(num);
-                System.out.println(num + ": pairs removed");
-            }
-            else {
-                nums.put(num, 1);
+        int length = A.length;
+        Arrays.sort(A);
+        for (int i=0; i < length;i+=2){
+            if(i+1 == length || A[i] != A[i+1]){
+                return A[i];
             }
         }
-        Set<Map.Entry<Integer, Integer>> entries = nums.entrySet();
-
-        for (Map.Entry<Integer, Integer> entry : entries) {
-            sinlge = entry.getKey();
-        }
-        return sinlge;
+        return 0;
     }
 }
