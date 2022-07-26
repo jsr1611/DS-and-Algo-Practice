8fb6b0ec0749145dab9020de1e5238107ea62aa3
Jumanazar <jsr1611@outlook.com>
2022-07-26 23:31:36 +0900

    12 ms - faster than 62.52% in Java

---
+++ Java/BinarySearch/CollisionDetection.java
@@ -0,0 +1,9 @@
+
+/**
+* author: Jumanazar Said
+* email: jumanazarsaidov@gmail.com
+* date: 2022/07/26 17:37
+*/
+
+public class CollisionDetection {
+}
---
+++ Java/BinarySearch/CompressString.java
@@ -0,0 +1,55 @@
+/**
+ * author: Jumanazar Said
+ * email: jumanazarsaidov@gmail.com
+ * date: 2022/07/26 18:19
+ * desc: Given a string lowercase alphabet s, eliminate consecutive duplicate characters from the string and return it.
+ * That is, if a list contains repeated characters, they should be replaced with a single copy of the character.
+ * The order of the elements should not be changed.
+ *
+ * Constraints
+ * 0 ≤ n ≤ 100,000 where n is the length of s
+ *
+ * Example
+ *
+ * Input
+ * s = "aaaaaabbbccccaaaaddf"
+ * Output
+ * "abcadf"
+ */
+
+public class CompressString {
+    public static void main(String[] args) {
+        System.out.println(new CompressString().solve("aaaaaabbbccccaaaaddf"));
+    }
+    public String solve(String s) {
+        int len = s.length()-1, counter = 0;
+        StringBuilder start = new StringBuilder();
+        StringBuilder end = new StringBuilder();
+
+        while (len/2 >= counter){
+            if(start.length() == 0){
+                start.append(s.charAt(0));
+            }
+            else if(start.charAt(start.length()-1) != s.charAt(counter)){
+                start.append(s.charAt(counter));
+            }
+
+            if(end.length() == 0){
+                end.append(s.charAt(len));
+            }
+            else if(end.charAt(end.length()-1) != s.charAt(len-counter)){
+                end.append(s.charAt(len-counter));
+            }
+            counter++;
+        }
+        end.reverse();
+        if(start.charAt(start.length()-1) != end.charAt(0)){
+            return start.append(end).toString();
+        }else {
+            if(end.length() > 1){
+                return start.append(end.substring(1)).toString();
+            }
+            return start.toString();
+        }
+    }
+}
