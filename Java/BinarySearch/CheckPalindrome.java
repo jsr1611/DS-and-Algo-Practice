7c2abab6f475e3a959b872f430045d526f867f9e
Jumanazar <jsr1611@outlook.com>
2022-07-26 16:19:49 +0900

    faster than 100% submission

--- Java/BinarySearch/CheckPalindrome.java
+++ Java/BinarySearch/CheckPalindrome.java
@@ -1,9 +1,20 @@
-
 /**
-* author: Jumanazar Said
-* email: jumanazarsaidov@gmail.com
-* date: 2022/07/25 13:46
-*/
+ * author: Jumanazar Said
+ * email: jumanazarsaidov@gmail.com
+ * date: 2022/07/25 13:46
+ */
 
-package PACKAGE_NAME;public class CheckPalindrome {
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        System.out.println(new CheckPalindrome().solve("olaamalo"));
+    }
+    public boolean solve(String s) {
+        for(int i=0; i < s.length()/2; i++){
+            if(s.charAt(i) != s.charAt(s.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
