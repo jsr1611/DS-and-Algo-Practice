c24a0a92acca5cf7ac575100da7004dad9cc609a
Jumanazar <jsr1611@outlook.com>
2022-07-28 16:35:22 +0900

    solution - 174ms, faster than 12%

---
+++ Java/BinarySearch/LargestNumberByTwoTimes.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * author: Jumanazar Said
+ * email: jumanazarsaidov@gmail.com
+ * date: 2022/07/28 16:19
+ * desc: Given a list of integers nums, return whether the largest number is bigger than the second-largest number by more than two times.
+ *
+ * Constraints
+ *
+ * 2 ≤ n ≤ 100,000 where n is the length of nums
+ * Input
+ *      nums = [3, 6, 9]
+ * Output
+ *      false
+ * Explanation
+ *      9 is not bigger than 2 * 6.
+ *
+ * Input
+ *      nums = [3, 6, 15]
+ * Output
+ *      true
+ * Explanation
+ *      15 is bigger than 12 (2 * 6).
+ */
+
+public class LargestNumberByTwoTimes {
+    public static void main(String[] args) {
+        int[] arr = {1,0};
+        System.out.println(solve(arr));
+    }
+    public static boolean solve(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length-2] * 2 < nums[nums.length-1];
+    }
+}
