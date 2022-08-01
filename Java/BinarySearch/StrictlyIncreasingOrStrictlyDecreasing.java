36062f16be68ab7dbe5b1661d5bba5c17b33ea5b
Jumanazar <jsr1611@outlook.com>
2022-08-02 01:30:55 +0900

    solution added - 75ms, faster than 64%

--- Java/BinarySearch/StrictlyIncreasingOrStrictlyDecreasing.java
+++ Java/BinarySearch/StrictlyIncreasingOrStrictlyDecreasing.java
@@ -1,9 +1,35 @@
+import java.util.Arrays;
 
 /**
-* author: Jumanazar Said
-* email: jumanazarsaidov@gmail.com
-* date: 2022/07/28 17:44
-*/
+ * author: Jumanazar Said
+ * email: jumanazarsaidov@gmail.com
+ * date: 2022/07/28 17:44
+ * desc: Given a list of integers nums, return whether the list is strictly increasing or strictly decreasing.
+ *
+ * Constraints
+ *
+ * n ≤ 100,000 where n is the length of nums
+ * Input
+ * nums = [1, 2, 3, 4, 5]
+ * Output
+ * true
+ * Explanation
+ * This is strictly increasing.
+ * Input
+ * nums = [1, 2, 3, 4, 5, 5]
+ * Output
+ * false
+ * Explanation
+ * Since there's two duplicate 5 this is not strictly increasing.
+ */
 
-package PACKAGE_NAME;public class StrictlyIncreasingOrStrictlyDecreasing {
+public class StrictlyIncreasingOrStrictlyDecreasing {
+    public static void main(String[] args) {
+
+    }
+    public boolean solve(int[] nums) {
+        Arrays.sort(nums);
+
+        return false;
+    }
 }
---
+++ Java/BinarySearch/StrictlyIncreasingStrictlyDecreasing.java
@@ -0,0 +1,9 @@
+
+/**
+* author: Jumanazar Said
+* email: jumanazarsaidov@gmail.com
+* date: 2022/08/02 00:23
+*/
+
+package PACKAGE_NAME;public class StrictlyIncreasingStrictlyDecreasing {
+}
