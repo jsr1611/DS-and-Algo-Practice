6e8a4b9bedd9a719c88d97e50290fd024de65b49
Jumanazar <jsr1611@outlook.com>
2022-08-06 14:37:13 +0900

    solution updated: 5ms, faster than 85%

--- Java/BinarySearch/SumOfFirstNOddInteger.java
+++ Java/BinarySearch/SumOfFirstNOddInteger.java
@@ -14,14 +14,6 @@ public class SumOfFirstNOddInteger {
         System.out.println(solve(5));
     }
     public static int solve(int n){
-        int total = 0;
-        int count = 1;
-        while (n > 0){
-            total += count;
-            System.out.println(total + " = " + count);
-            count+=2;
-            n-=1;
-        }
-        return total;
+        return n * n;
     }
 }
