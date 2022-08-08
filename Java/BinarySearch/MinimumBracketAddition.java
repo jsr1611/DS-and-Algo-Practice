10f060c9954f44cdc15bbfd7569215a0c19940d2
Jumanazar <jsr1611@outlook.com>
2022-08-08 23:55:16 +0900

    solution added 40ms, faster than 19%

---
+++ Java/BinarySearch/MinimumBracketAddition.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+/**
+ * author: Jumanazar Said
+ * email: jumanazarsaidov@gmail.com
+ * date: 2022/08/08 23:54
+ */
+
+public class MinimumBracketAddition {
+    public int solve(String s) {
+        int count = 0;
+        int i = 0;
+        Stack<Character> stack = new Stack<>();
+        char c;
+        while(s.length() > i){
+            c = s.charAt(i);
+            if(c == ')'){
+                if(stack.size() != 0 && stack.peek() == '(')
+                    stack.pop();
+                else
+                    stack.push(c);
+            }
+            else{
+                stack.push(c);
+            }
+            i++;
+        }
+        return stack.size();
+}
