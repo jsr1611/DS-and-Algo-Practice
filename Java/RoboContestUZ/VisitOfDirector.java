c3a7dfdae72ebd8161eb4735b33035f4ea720bca
Jumanazar <jsr1611@outlook.com>
2022-08-11 12:01:16 +0900

    solution added, Director Visit

--- Java/RoboContestUZ/VisitOfDirector.java
+++ Java/RoboContestUZ/VisitOfDirector.java
@@ -1,9 +1,22 @@
-
 /**
-* author: Jumanazar Said
-* email: jumanazarsaidov@gmail.com
-* date: 2022/08/11 02:50
-*/
+ * author: Jumanazar Said
+ * email: jumanazarsaidov@gmail.com
+ * date: 2022/08/11 02:50
+ */
 
-package PACKAGE_NAME;public class VisitOfDirector {
+import java.util.*;
+import java.io.*;
+
+public class VisitOfDirector {
+        public static void main(String[] argv) throws IOException{
+            new VisitOfDirector().run();
+        }
+        PrintWriter pw;
+        Scanner sc;
+    public void run() throws IOException{
+        sc = new Scanner(new File("input.txt"));
+        pw = new PrintWriter(new File("output.txt"));
+        pw.print((long) sc.nextInt() * sc.nextInt());
+        pw.close();
+    }
 }
