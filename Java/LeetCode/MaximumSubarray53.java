15f5ed26f50c152613852bc48e549218a589c23f
Jumanazar <jsr1611@outlook.com>
2022-09-01 09:59:40 +0900

    problem declaration

--- Java/LeetCode/MaximumSubarray53.java
+++ Java/LeetCode/MaximumSubarray53.java
@@ -1,9 +1,43 @@
-
 /**
-* author: Jumanazar Said
-* email: jumanazarsaidov@gmail.com
-* date: 2022/09/01 09:52
-*/
+ * author: Jumanazar Said
+ * email: jumanazarsaidov@gmail.com
+ * date: 2022/09/01 09:52
+ * desc:
+ * 53. Maximum Subarray
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has
+ * the largest sum and return its sum.
+ *
+ * A subarray is a contiguous part of an array.
+ * Related topics: Array, Divide and Conquer, Dynamic Programming
+ * Example 1:
+ *
+ * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Example 2:
+ *
+ * Input: nums = [1]
+ * Output: 1
+ * Example 3:
+ *
+ * Input: nums = [5,4,-1,7,8]
+ * Output: 23
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 105
+ * -104 <= nums[i] <= 104
+ *
+ *
+ * Follow up: If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ */
 
-package PACKAGE_NAME;public class MaximumSubarray53 {
+public class MaximumSubarray53 {
+    public int maxSubArray(int[] nums) {
+        int max = 0;
+
+        return max;
+    }
+
 }
