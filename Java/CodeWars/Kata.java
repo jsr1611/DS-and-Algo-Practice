450c77d7ee06b988cf02598bfbb2e816da1113b3
jimhome_mac <jsr1611@outlook.com>
2023-05-24 16:35:49 +0900

    added solutions for some problems

--- Java/CodeWars/Kata.java
+++ Java/CodeWars/Kata.java
@@ -16,4 +16,26 @@ public class Kata
         Arrays.sort(a);
         return a[a.length - 1] <= limit;
     }
+
+    public static int[] digitize(long n) {
+        // Code here
+        // problem: https://www.codewars.com/kata/5583090cbe83f4fd8c000051/java
+
+        //solution(s):
+        String numStr = n + "";
+        int length = numStr.length();
+        int[] array = new int[length];
+        for(int i=0; i < length; i++){
+            array[length-1-i] = numStr.charAt(i) - 48;
+        }
+        return array;
+    }
+
+    public static double findUniq(double arr[]) {
+        //problem: https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/java
+        // solution(s):
+        // Do the magic
+        Arrays.sort(arr);
+        return arr[0] == arr[1] ? arr[arr.length-1]: arr[0];
+    }
 }
