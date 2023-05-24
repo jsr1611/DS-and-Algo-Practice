347a3bef0cd7e24f12411826285905952492b0e2
jimhome_mac <jsr1611@outlook.com>
2023-05-24 16:04:29 +0900

    CodeWars problem description updated

--- Java/CodeWars/Kata.java
+++ Java/CodeWars/Kata.java
@@ -3,6 +3,9 @@ public class Kata
 {
     public static boolean smallEnough(int[] a, int limit)
     {
+        // problem: https://www.codewars.com/kata/57cc981a58da9e302a000214/java
+
+        // solution(s):
         // brute-force
         //     for(int num: a){
         //       if(num > limit) return false;
