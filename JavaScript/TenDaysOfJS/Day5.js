1b722963afe9df2c942e34114864864da102cbbc
Jumanazar <jimmy@sweetk.co.kr>
2023-06-05 13:30:04 +0900

    arrow function to modify an array

--- JavaScript/TenDaysOfJS/Day5.js
+++ JavaScript/TenDaysOfJS/Day5.js
@@ -43,3 +43,42 @@ class Square extends Rectangle{
         this.a = a;
     }
 }
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ *
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+
+    let s1 = (expressions[1] + Math.sqrt(Math.pow(expressions[1], 2) - 16 * expressions[0])) / 4;
+    let s2 = (expressions[1] - Math.sqrt(Math.pow(expressions[1], 2) - 16 * expressions[0])) / 4;
+    return s1 > s2 ? [s2, s1] : [s1,s2];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ *
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    //map creates new array
+    let newArr = nums.map((x) => {
+        if(x % 2 === 0) x*=2;
+        else x*=3;
+        return x;
+    });
+
+    //forEach mutates the original array objects, but not primitives
+    //this will not work!
+    // nums.forEach(x => {
+    //     if(x % 2 === 0) x*=2;
+    //     else x*=3;
+    // });
+    return newArr;
+}
