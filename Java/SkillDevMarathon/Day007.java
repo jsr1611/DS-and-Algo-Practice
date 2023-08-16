b24b09dc29d7d983961c90513d31e5d9ac4eb7c1
Jumanazar Saidov <jsr1611@outlook.com>
2023-08-16 16:16:38 +0500

    added day7 rotate image solution

--- Java/SkillDevMarathon/Day007.java
+++ Java/SkillDevMarathon/Day007.java
@@ -11,17 +11,46 @@ public class Day007 {
         if(num < 10) return 1;
         int count = 0;
         int cpNum = num;
-        int length = (num + "").length();
         int sufNum = 0;
 
-        while (length > 0){
+        while (cpNum > 0){
             sufNum = cpNum % 10;
             if(num % sufNum == 0){
                 count++;
             }
             cpNum /= 10;
-            length--;
         }
         return count;
+    }
+
+    /*
+    @description 48. Rotate Image
+    @author jsr1611
+    @reference https://leetcode.com/problems/rotate-image/
+     */
+    public void rotate(int[][] matrix){
+        /* you have to rotate the image in-place, don't create another matrix */
+        int n = matrix.length;
+        // column reverse
+        for(int i=0; i < n; i++){
+            int a = 0;
+            int b = n-1;
+            while(a <= b){
+                int tmp = matrix[a][i];
+                matrix[a][i] = matrix[b][i];
+                matrix[b][i] = tmp;
+                a++;
+                b--;
+            }
+        }
+
+        //transpose
+        for (int i=0; i < n; i++){
+            for (int j=i+1; j < n; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
     }
 }
