fd7fffcb7253ce29d28d519070e52401812bb00c
Jumanazar Saidov <jsr1611@outlook.com>
2023-08-18 18:23:37 +0500

    added Day009 Encode and decode solution added

--- Java/SkillDevMarathon/Day009.java
+++ Java/SkillDevMarathon/Day009.java
@@ -1,8 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Day009 {
     /*
     @description 1108. Defanging an IP address
     @author jsr1611
-    @reference https://leetcode.com/problems/find-triangular-sum-of-an-array/
+    @reference https://leetcode.com/problems/defanging-an-ip-address/
      */
     public String defangIPaddr(String address){
         int len = address.length();
@@ -15,5 +19,32 @@ public class Day009 {
             }
         }
         return defAddr.toString();
+    }
+
+    public String CHARS =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    public Map<String, String> store = new HashMap<>();
+
+    /*
+    @description 535. Encode and Decode TinyURL
+    @author jsr1611
+    @reference https://leetcode.com/problems/encode-and-decode-tinyurl/
+    // Encodes a URL to a shortened URL.
+     */
+    public String encode(String longUrl){
+        StringBuilder link = new StringBuilder();
+        Random rnd = new Random();
+        String tinyUrl;
+        while (link.length() < 7){
+            int index = (int) (rnd.nextFloat() * CHARS.length());
+            link.append(CHARS.charAt(index));
+        }
+        tinyUrl = rnd.toString();
+        store.put(tinyUrl, longUrl);
+        return tinyUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl){
+        return store.get(shortUrl);
     }
 }
