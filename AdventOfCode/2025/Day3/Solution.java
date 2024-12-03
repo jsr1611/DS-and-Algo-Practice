a6d3fdc977c97819e28c0db5a9ad01405e3490ff
Jumanazar Saidov <sjumanazar@autoeveramerica.com>
2024-12-03 19:21:57 +0900

    Advent of Code Day 3 solution 2 submitted and abstracted part 1 to be reused for part 2

--- AdventOfCode/2025/Day3/Solution.java
+++ AdventOfCode/2025/Day3/Solution.java
@@ -9,37 +9,62 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
+    private static int counter;
     public static void main(String[] args) {
-        System.out.println(solve(1));
+        counter = 0;
+//        System.out.println(solve(1));
+//        counter = 0;
+        System.out.println(solve(2));
     }
 
     private static long solve(int partNumber) {
         String fileName = "AdventOfCode/2025/Day3/input.txt";
         long totalCount = 0;
-        int counter = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             System.out.println("File Content:");
+            Boolean flag = null;
             while ((line = reader.readLine()) != null) {
-                Pattern pattern = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)");
-                Matcher matcher = pattern.matcher(line);
-                while (matcher.find()) {
-                    String mul = matcher.group();   //mul(num1,num2)
-                    Pattern numPattern = Pattern.compile("\\d{1,3}");
-                    Matcher mulMatcher = numPattern.matcher(mul);
-                    List<Integer> nums = new ArrayList<>();
-                    while (mulMatcher.find()){
-                        nums.add(Integer.parseInt(mulMatcher.group())); //num
+                if(partNumber == 2) { //part 2
+                    StringBuilder sb = new StringBuilder();
+                    String[] dondonts = line.split("(?=don't\\(\\))|(?=do\\(\\))");
+                    for (String extract : dondonts) {
+                        if(extract.startsWith("don't()") || (flag != null && !flag && !extract.startsWith("do()"))){
+//                            System.out.println("DONTS [" + flag + "]  " + extract);
+                            flag = false;
+                        }else {
+//                            System.out.println("DOS:  ["+flag+"]  " + extract);
+                            flag = true;
+                            sb.append(extract);
+                        }
                     }
-                    counter++;
-                    totalCount += (nums.get(0) * nums.get(1));
-
-                System.out.println(String.format("No %d totalCount: %d, numbers: %d, %d", counter, totalCount,nums.get(0), nums.get(1)));
+                    line = sb.toString();
                 }
+                totalCount += mul(line); // part 1
+
             }
             return totalCount;
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        return totalCount;
+    }
+
+    private static long mul(String line){
+        long totalCount = 0;
+        Pattern pattern = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)");
+        Matcher matcher = pattern.matcher(line);
+        while (matcher.find()) {
+            String mul = matcher.group();   //mul(num1,num2)
+            Pattern numPattern = Pattern.compile("\\d{1,3}");
+            Matcher mulMatcher = numPattern.matcher(mul);
+            List<Integer> nums = new ArrayList<>();
+            while (mulMatcher.find()) {
+                nums.add(Integer.parseInt(mulMatcher.group())); //num
+            }
+            counter++;
+            totalCount += (nums.get(0) * nums.get(1));
+            System.out.println(String.format("No %d totalCount: %d, numbers: %d, %d", counter, totalCount, nums.get(0), nums.get(1)));
         }
         return totalCount;
     }
